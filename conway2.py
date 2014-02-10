a0dd58eb19843eca43d03a1eb9f069ad4459e290
ThomasMW <ThomasM@emerge-it.co.uk>
2014-02-10 14:17:22 +0000

    fix tick method

--- conway2.py
+++ conway2.py
@@ -2,17 +2,20 @@ class board:
     """\
     class to represent a Conway's Game of Life board\
     """ 
-    size = 0
-    cells = []
-    
     def __init__(self, square): 
+        self.size = 0
+        self.cells = []
         self.size = square 
         for idx in range(0, self.size): 
             x = []
             for jdx in range(0, self.size):
                 x.append(Cell(idx, jdx))
             self.cells.append(x)
- ###
+
+    def seed(self, coords):
+        self.cells[coords[1]][coords[0]].is_alive = True 
+
+ 
     def state(self):
         """\
         check the state of the current_board\
@@ -25,39 +28,31 @@ class board:
             ret += "\n"
         return ret 
 
-    def tick(self): 
-        """\
-        iterate board by one generation\
-        """
-        new_board = board(self.size)
-        print new_board.state()
-
-        for col in self.cells:
-            for cell in col: 
-                    new_board.cells[cell.x][cell.y].is_alive = self.alive_next(
-                        cell)
-                    
-        #check & iterate state of all cells on board.
-
-        self = new_board
-
     def _check_adjacent(self, cell):
+
+        def wrap(index):
+            """\
+            Allows toroidal implementation by giving a wrapped value for input \
+            index\
+            """
+
+            return index % self.size
+
         """\
         checks the 8 cells directly adjacent to the cell passed in, returning 
         count of adjacent live cells.\
         """
-        print cell.x , ", ", cell.y
-        print self.state()
+        
         ret = 0
         #board instance needs to be referenced here for things to be done.
-        ret += self.cells[cell.x + 1][cell.y].is_alive
-        ret += self.cells[cell.x - 1][cell.y].is_alive
-        ret += self.cells[cell.x + 1][cell.y + 1].is_alive
-        ret += self.cells[cell.x - 1][cell.y + 1].is_alive
-        ret += self.cells[cell.x + 1][cell.y - 1].is_alive
-        ret += self.cells[cell.x - 1][cell.y - 1].is_alive
-        ret += self.cells[cell.x][cell.y + 1].is_alive
-        ret += self.cells[cell.x][cell.y - 1].is_alive
+        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y)].is_alive
+        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y)].is_alive
+        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y + 1)].is_alive
+        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y + 1)].is_alive
+        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y - 1)].is_alive
+        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y - 1)].is_alive
+        ret += self.cells[wrap(cell.x)][wrap(cell.y + 1)].is_alive
+        ret += self.cells[wrap(cell.x)][wrap(cell.y - 1)].is_alive
         #idx % len(board.cells(coord))
         return ret
 
@@ -66,6 +61,7 @@ class board:
         determines a cell's new state based on adjacent_living cells.\
         """
         adjacent_living = self._check_adjacent(cell)
+        
         if adjacent_living < 2 or adjacent_living > 3: 
             return False
         else: 
@@ -85,7 +81,24 @@ class Cell:
         self.y = y
 
 
-   
+def tick(tick_board): 
+    """\
+    iterate board by one generation\
+    """
+    new_board = board(tick_board.size)
+    for col in tick_board.cells:
+        for cell in col: 
+                new_board.cells[cell.x][cell.y].is_alive = tick_board.alive_next(cell)
+                    
+    #check & iterate state of all cells on board.
+    print "old board:"
+    print tick_board.state() 
+    print "new board:"
+    print new_board.state()
+    return new_board  
 
     
-x = board(3)
+x = board(10)
+x.seed([0,1])
+x.seed([1,1])
+x.seed([1,2])
