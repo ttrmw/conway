d10fa46e1cebb0e13c7833214fb0bcdb8c60db87
ttrmw <thomasm@emerge-it.co.uk>
2014-06-10 20:09:32 +0100

    refactor check_adjacent

--- conway2.py
+++ conway2.py
@@ -7,52 +7,52 @@ class Board:
         self.cells = []
         self.size = square 
         for idx in range(0, self.size): 
-            x = []
+            y = []
             for jdx in range(0, self.size):
-                x.append(Cell(idx, jdx))
-            self.cells.append(x)
+                y.append(Cell(idx, jdx))
+            self.cells.append(y)
 
     def seed(self, coords):
-        self.cells[coords[1]][coords[0]].is_alive = True 
+        self.cells[coords[0]][coords[1]].is_alive = True
 
     def state(self):
         """\
         check the state of the current_board\
         """
         ret = ""
+        ret += "-" * (1 + self.size)
+        ret += "\n"
+
         for col in self.cells: 
             for cell in col: 
-                ret += "".join(("(", str(cell.y), ",", str(cell.x), ")", ": ", 
-                       str(cell.is_alive), "  "))
+                if cell.x == 0 or cell.x == self.size - 1:
+                    ret += "|"
+                if cell.is_alive: 
+                    ret += "x"
+                else:
+                    ret += " "
             ret += "\n"
+
+        ret += "-" * (1 + self.size)
         return ret 
 
     def _check_adjacent(self, cell):
+        """\
+        checks the 8 cells directly adjacent to the cell passed in, returning
+        count of adjacent live cells.\
+        """
 
         def wrap(index):
             """\
             Allows toroidal implementation by giving a wrapped value for input \
             index\
             """
-
             return index % self.size
 
-        """\
-        checks the 8 cells directly adjacent to the cell passed in, returning 
-        count of adjacent live cells.\
-        """
-        
-        ret = 0
-        
-        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y)].is_alive
-        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y)].is_alive
-        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y + 1)].is_alive
-        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y + 1)].is_alive
-        ret += self.cells[wrap(cell.x + 1)][wrap(cell.y - 1)].is_alive
-        ret += self.cells[wrap(cell.x - 1)][wrap(cell.y - 1)].is_alive
-        ret += self.cells[wrap(cell.x)][wrap(cell.y + 1)].is_alive
-        ret += self.cells[wrap(cell.x)][wrap(cell.y - 1)].is_alive
-        
+        ret = -1    # discard checked cell's status
+        for j in range(-1, 2):
+            for i in range(-1, 2):
+                ret += self.cells[wrap(cell.x + i)][wrap(cell.y + j)].is_alive
         return ret
 
     def alive_next(self, cell): 
@@ -68,7 +68,6 @@ class Board:
         else:
             return False
 
-        return True
 
 class Cell:
     """\
@@ -78,7 +77,7 @@ class Cell:
     y = 0
     is_alive = False 
 
-    def __init__(self, x, y):
+    def __init__(self, y, x):
         self.x = x
         self.y = y
 
@@ -94,31 +93,31 @@ def tick(tick_board):
                 new_board.cells[cell.x][cell.y].is_alive = tick_board.alive_next(cell)
                 i += new_board.cells[cell.x][cell.y].is_alive
 
-    
-    print "old board:"
-    print tick_board.state() 
+    #print "old board:"
+    #print tick_board.state()
     print "new board:"
     print new_board.state()
     print i
     return new_board  
 
     
-x = Board(10)
-x.seed([0, 1])
-x.seed([3, 1])
-x.seed([2, 2])
+game_board = Board(10)
+game_board.seed([0, 1])
+game_board.seed([3, 1])
+game_board.seed([2, 2])
+game_board.seed([1, 1])
+game_board.seed([3, 2])
+game_board.seed([2, 1])
+game_board.seed([4, 1])
+game_board.seed([0, 0])
+game_board.seed([2, 1])
+game_board.seed([4, 0])
+game_board.seed([4, 2])
+game_board.seed([4, 3])
+game_board.seed([4, 1])
+game_board.seed([3, 4])
+game_board.seed([2, 4])
 
-x.seed([1, 1])
-x.seed([3, 2])
-x.seed([2, 1])
-x.seed([4, 1])
-x.seed([0, 0])
-x.seed([2, 1])
-
-x.seed([4, 0])
-x.seed([4, 2])
-x.seed([4, 3])
-x.seed([4, 1])
-x.seed([0, 4])
-x.seed([2, 4])
-
+print game_board.state()
+for i in range(0, 20):
+    game_board = tick(game_board)
